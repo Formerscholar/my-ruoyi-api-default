6f0e6a3034fff885fa8f04dd53cef40c2262a941
Formerscholar <2319315514@qq.com>
2021-09-28 21:19:54 +0800

    权限

---
+++ chad-admin/src/test/java/com/ruoyi/RuoYiApplicationTest.java
@@ -0,0 +1,34 @@
+package com.chad;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * RuoYiApplication Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>9�� 27, 2021</pre>
+ */
+public class RuoYiApplicationTest {
+	
+	@Before
+	public void before() throws Exception {
+	}
+	
+	@After
+	public void after() throws Exception {
+	}
+	
+	/**
+	 * Method: main(String[] args)
+	 */
+	@Test
+	public void testMain() throws Exception {
+//TODO: Test goes here... 
+		System.out.println("true = " + true);
+	}
+	
+	
+} 
